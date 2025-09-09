504c8e76ff0aadb7d514c4cfd09b0eea3e4db5f0
ikariLain <matheus-torrico@outlook.com>
2025-09-09 12:13:07 +0200

    Update ServiceOrderPatchDTO

--- DTOs/ServiceOrder/ServiceOrderPatchDTO.cs
+++ DTOs/ServiceOrder/ServiceOrderPatchDTO.cs
@@ -5,14 +5,13 @@ namespace RestaurantAPI.DTOs.ServiceOrder
     public class ServiceOrderPatchDTO
     {
         public int? FoodId_FK { get; set; }
-
-        public int Reservation_FK { get; set; }
+        public int? Reservation_FK { get; set; }
 
         [Range(0, 50000, ErrorMessage = "Total price must be between 0 and 50000")]
-        public decimal TotalPriceAmount { get; set; }
+        public decimal? TotalPriceAmount { get; set; }
 
         [Range(1, 100, ErrorMessage = "Quantity must be between 1 and 100")]
-        public int Quantity { get; set; }
+        public int? Quantity { get; set; }
 
         [MaxLength(500, ErrorMessage = "Note can only contain 500 characters")]
         public string? Note { get; set; }
